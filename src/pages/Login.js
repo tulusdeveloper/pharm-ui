491a793f601cad6727257caccddef627ac07600c
tulusdeveloper <tulusdeveloper@gmail.com>
2023-11-22 11:06:29 +0300

    changes on Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,54 +3,74 @@ import 'adminbsb-materialdesign/plugins/bootstrap/css/bootstrap.css';
 import 'adminbsb-materialdesign/plugins/node-waves/waves.css';
 import 'adminbsb-materialdesign/plugins/animate-css/animate.css';
 import 'adminbsb-materialdesign/css/style.css';
+import GoogleFontLoader from 'react-google-fonts';
+import 'material-icons/iconfont/material-icons.css';
 
 
 
 class Login extends React.Component {
 
     render() {
-        document.body.className="login-page";
+        document.body.className = "login-page";
 
         return (
-            <div class="login-box">
-                <div class="logo">
-                    {/* <a href="#">Admin<b>BSB</b></a> */}
+            <React.Fragment>
+            <GoogleFontLoader
+            fonts={[
+                {
+                font: 'Roboto',
+                weights: [400, '700'],
+                }
+            ]}
+            subsets={['latin', 'cyrillic-ext']}
+            />
+             <GoogleFontLoader
+            fonts={[
+                {
+                font: 'Material+Icons'
+                }
+            ]}
+            />
+
+            <div className="login-box">
+                <div className="logo">
+                    <a href="#">Admin<b>BSB</b></a>
                     <small>Admin BootStrap Based - Material Design</small>
                 </div>
-                <div class="card">
-                    <div class="body">
+                <div className="card">
+                    <div className="body">
                         <form id="sign_in" method="POST">
-                            <div class="msg">Sign in to start your session</div>
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    <i class="material-icons">person</i>
+                            <div className="msg">Sign in to start your session</div>
+                            <div className="input-group">
+                                <span className="input-group-addon">
+                                    <i className="material-icons">person</i>
                                 </span>
-                                <div class="form-line">
-                                    <input type="text" class="form-control" name="username" placeholder="Username" required autoFocus/>
+                                <div className="form-line">
+                                    <input type="text" className="form-control" name="username" placeholder="Username" required autoFocus/>
                                 </div>
                             </div>
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    <i class="material-icons">lock</i>
+                            <div className="input-group">
+                                <span className="input-group-addon">
+                                    <i className="material-icons">lock</i>
                                 </span>
-                                <div class="form-line">
-                                    <input type="password" class="form-control" name="password" placeholder="Password" required/>
+                                <div className="form-line">
+                                    <input type="password" className="form-control" name="password" placeholder="Password" required/>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-xs-8 p-t-5">
-                                    <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink"/>
+                            <div className="row">
+                                <div className="col-xs-8 p-t-5">
+                                    <input type="checkbox" name="rememberme" id="rememberme" className="filled-in chk-col-pink"/>
                                         <label for="rememberme">Remember Me</label>
                                 </div>
-                                <div class="col-xs-4">
-                                    <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
+                                <div className="col-xs-4">
+                                    <button className="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
                                 </div>
                             </div>
-                            <div class="row m-t-15 m-b--20">
-                                <div class="col-xs-6">
+                            <div className="row m-t-15 m-b--20">
+                                <div className="col-xs-6">
                                     <a href="sign-up.html">Register Now!</a>
                                 </div>
-                                <div class="col-xs-6 align-right">
+                                <div className="col-xs-6 align-right">
                                     <a href="forgot-password.html">Forgot Password?</a>
                                 </div>
                             </div>
@@ -58,7 +78,9 @@ class Login extends React.Component {
                     </div>
                 </div>
             </div>
+            </React.Fragment>
         )
+    
     }
 }
 
