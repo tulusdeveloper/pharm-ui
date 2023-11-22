43305eef7343435cb980221996b429cfa7ac2879
tulusdeveloper <tulusdeveloper@gmail.com>
2023-11-22 10:12:24 +0300

    added login page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
-ReactDOM.render(<h1>Hello</h1>,document.getElementById("root"))
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Login />}></Route>
+    </Routes>
+  </Router>
+);
